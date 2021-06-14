1e8359c45cb5f266137cabda5024bcd07d23d22d
Sanjay Patel <patelsan@pointclickcare.com>
2021-06-14 13:38:01 -0400

    added chart component to APP

--- src/components/Charts/Chars.jsx
+++ src/components/Charts/Chars.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import "./Charts.css"
 import {ChartBar} from "./ChartBar"
-import data from "bootstrap/js/src/dom/data";
-export const Chars = (props) =>
+
+export const Chart = (props) =>
 {
+    let bars;
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
+
+    if(props.dataPoints.length > 0)
+    {
+        bars = props.dataPoints.map((dataPoint) =>
+            <ChartBar
+                key={dataPoint.label}
+                value={dataPoint.value}
+                maxValue={totalMaximum}
+                label={dataPoint.label}
+            />
+        );
+    }
     return (
         <div className="chart">
-            {props.dataPoints.map(
-                (dataPoint) => {
-                    <ChartBar
-                        key={dataPoint.label}
-                        value={dataPoint.value}
-                        maxValue={null}
-                        label={dataPoint.label}
-                    />
-                }
-                )};
+            {bars}
         </div>
     )
 }
