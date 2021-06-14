098a84f7b3d939e35dbbc1607336e61f8bca0ca0
Sanjay Patel <patelsan@pointclickcare.com>
2021-06-14 13:38:18 -0400

    added chart component

--- src/components/Charts/ChartBar.js
+++ src/components/Charts/ChartBar.js
@@ -3,11 +3,15 @@ import "./ChartBar.css";
 
 export const ChartBar = (props) =>
 {
-    let calculateExpenses = Math.round(props.value - props.maxValue * 100) + "%";
+    let calculateExpensesHeight;
+    if(props.maxValue > 0) {
+        calculateExpensesHeight = Math.round(props.value / props.maxValue * 100) + "%";
+    }
+
     return(
         <div className="chart-bar">
             <div className="chart-bar__inner">
-                <div className="chart-bar__fill">{calculateExpenses}</div>
+                <div className="chart-bar__fill" style={{height:calculateExpensesHeight}}>{calculateExpensesHeight}</div>
             </div>
             <div className="chart-bar__label">
                 {props.label}
